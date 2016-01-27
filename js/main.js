b4bb4e753c4f56b6fba6edfca1d78fb965b5e6cc
Vicken <vicken.liu@gmail.com>
2016-01-27 15:53:19 +1300

    applied float to style the index page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
   
-  $('#menu-icon').click(function(){
+  $('#menuicon').click(function(){
     var icon=$(this);
-    $('#callaps').slideToggle(500,function(){
-      icon.find('.menuspan').toggleClass('callaps');
-      if(icon.find('.menuspan').hasClass('callaps')){
-        icon.find('h2').text('CLOSE');
-       }
-      else{
-        $('#content').fadeOut();
-        icon.find('h2').text('MENU');
-        $('#callaps').find('a').removeClass('active');}
+    $('#header').find('ul').slideToggle(500,function(){
+      icon.toggleClass('callaps');
     });
   });
 
-  $('#callaps').find('a').click(function(e){
+
+
+
+
+
+
+
+/*  $('#callaps').find('a').click(function(e){
     e.preventDefault();
     var fn=$(this).data('fn');
     $('#callaps').find('a').removeClass('active');
@@ -44,5 +44,5 @@ $(document).ready(function(){
     $('#menu-icon').trigger('click');
     $('#content').fadeIn();
     $('#callaps').find("a[data-fn='content']").addClass('active');
-  });
+  });*/
 });
